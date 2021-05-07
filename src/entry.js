c239aea05bb7addc560378cf72a5697a4b16eea5
deliaconstantino <dconstan92@gmail.com>
2021-05-07 18:17:41 -0400

    load journal entries in nested grid with tree image along right side of grid

--- src/entry.js
+++ src/entry.js
@@ -10,11 +10,13 @@ class Entry {
   static entriesContainer = document.getElementById("entries-container");
 
   render() {
+    let div = document.createElement('div');
     let p = document.createElement("p");
     // p.innerText = this.body + `   Time interval: ${this.timeInterval}`
     p.innerHTML = `
       ${this.body} <i> Time interval: ${this.timeInterval} </i>
     `
-    Entry.entriesContainer.appendChild(p);
+    div.appendChild(p);
+    Entry.entriesContainer.appendChild(div);
   }
 }
