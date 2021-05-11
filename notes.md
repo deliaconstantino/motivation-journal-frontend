af472926753a28b3d85b7426cd62fd1bf2630d4c
deliaconstantino <dconstan92@gmail.com>
2021-05-10 21:36:49 -0400

    added timer functionality and create entry action

--- notes.md
+++ notes.md
@@ -2,3 +2,10 @@
 
 * change nav-bar hamburger toggle to just show lemon image when smaller screen (not hamburger icon)
 * change Motivation Journal/navbar text to melon color
+* modal for new journal entry
+* set form for modal to persist journal entries
+  - connect to backend
+* created timer functionality
+  - create dropdown to choose integer amount of time 1 - 30 min
+  - create a settimeout method to timeout t that point
+  - have save or delete populate at end
--- src/entry.js
+++ src/entry.js
@@ -8,15 +8,74 @@ class Entry {
 
   static all = [];
   static entriesContainer = document.getElementById("entries-container");
+  static ul = document.createElement('ul');
+  static newEntryForm = document.querySelector("#new-entry-form");
+  static timerButton = document.querySelector("#timer-button");
+  static timerSelect = document.querySelector("#timer-select");
+
+
+
+  static createEntriesUl() {
+    Entry.entriesContainer.appendChild(Entry.ul);
+    Entry.newEntryForm.addEventListener("submit", Entry.createNewEntry);
+    Entry.timerButton.addEventListener("click", Entry.setTimer)
+
+  }
+
 
   render() {
-    let ul = document.createElement('ul');
     let li = document.createElement("li");
-    // p.innerText = this.body + `   Time interval: ${this.timeInterval}`
     li.innerHTML = `
       <i> Time interval: ${this.timeInterval} </i> ${this.body}
     `
-    ul.appendChild(li);
-    Entry.entriesContainer.appendChild(ul);
+    Entry.ul.appendChild(li);
   }
+
+
+
+static interval;
+static numMille;
+
+static setTimer(e) {
+  console.log(e.target);
+  Entry.numMille = parseInt(Entry.timerSelect.value, 10) * 1000; // TODO: change to 60000 (1000 mille/second)
+  Entry.interval = setTimeout(Entry.saveOrDeleteEntry, Entry.numMille)
+
+}
+
+ static createNewEntry(e) {
+    e.preventDefault()
+    let body = e.target.querySelector("#body");
+
+    let data = {
+      body: body.value,
+      time_interval: Entry.timerSelect.value
+    }
+
+    let configObj = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+
+    fetch(`${baseEntriesURL}`, configObj)
+    .then(resp => resp.json())
+    .then(e => {
+      console.log(e);
+      let entry = new Entry(e.id, e.body, e.time_interval);
+      entry.render();
+      let modal = document.querySelector("#staticBackdrop");
+      clearTimeout(Entry.interval);
+      body.value = "";
+      alert('Journal entry saved!')
+    })
+
+  }
+
+  static saveOrDeleteEntry() {
+    alert(`Your ${Entry.timerSelect.value} minute writing timer is up! Don't forget to click the 'save' button below if you'd like to keep your entry.`);
+  }
+
 }
