63c5b673dea7e0f7f8bcf8c5f1085902e632dbd3
Delia Constantino <dconstan92@gmail.com>
2021-09-03 17:00:06 -0400

    updated url for deployed reference

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // const baseURL = "http://localhost:3000/api/v1"
-const baseURL = `https://protected-ocean-94869.herokuapp.com/${api/v1}`
+const baseURL = "https://protected-ocean-94869.herokuapp.com/api/v1"
 // const baseEntriesURL = "http://localhost:3000/api/v1/entries"
-const baseEntriesURL = `https://protected-ocean-94869.herokuapp.com/${api/v1/entries}`
+const baseEntriesURL = "https://protected-ocean-94869.herokuapp.com/api/v1/entries"
 
 let adapter = new Adapter(baseURL)
 adapter.loadContent()
